29a1f9ce4e8518210db1887429c811adb557d6da
sanjayk63806-sank <sanjayk63806@gmail.com>
2025-12-05 10:25:51 +0530

    Add files via upload
    day 4

---
+++ Mobile.java
@@ -0,0 +1,12 @@
+package learnautomation;
+
+public class Mobile {
+	public void takePhoto() {
+		System.out.println("take photo");
+	}
+	public static void main(String[] args) {
+		Mobile obj=new Mobile();
+		obj.takePhoto();
+	}
+
+}
